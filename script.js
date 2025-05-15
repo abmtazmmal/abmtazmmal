631a2a177890f97d5118e5b62f822c7c8bf86584
ABUL BASHAR MUHAMMAD TAZMMAL HAQ <abmtazmmal@gmail.com>
2025-05-15 22:33:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for contacting. We will get back to you soon.");
+  });
+  
